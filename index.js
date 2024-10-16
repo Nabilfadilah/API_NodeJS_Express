5d1460737219b0676d18fd7dc5c9355a41b21303
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-16 10:26:39 +0700

    Menambahkan module body parser, menambah route method post dan put - video 2

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express') // module
+const bodyParser = require('body-parser')
 const app = express() // variabel dengan module express
 const port = 3000 // variabel mengatur port
+
+app.use(bodyParser.json())
 
 // route/url/endpoint utama ("/")
 // mengambil function app dengan method get
 app.get('/', (req, res) => { // yang menerima callback req dan res
   // get akan mengirim response ke browser yang nge request  
-  res.send('Hello World NGABBERS Selalu dihati!')
+  res.send('Hello semuanya Selalu dihati!')
 })
+
+app.post('/login', (req, res) => {
+    console.log({ requestFromOutside: req.body })
+    res.send('login berhasil')
+})
+
+app.put('/username', (req, res) => {
+    console.log({updateData: req.body})
+    res.send('Update berhasil')
+})
+
+// req : ngambil dari luar ke dalam, terpanggil atau tidak 
+// res : ngirim keluar
 
 // untuk me running dibalik layar
 app.listen(port, () => {
