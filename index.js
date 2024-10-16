e5f6703556970f74c3406b37af5a427c79a49fd0
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-16 14:27:46 +0700

    Implementasi database dah membauat route - video 4

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express') // module
 const bodyParser = require('body-parser')
+const db = require('./db')
+const response = require('./response')
+
 const app = express() // variabel dengan module express
 const port = 3000 // variabel mengatur port
 
@@ -9,7 +12,28 @@ app.use(bodyParser.json())
 // mengambil function app dengan method get
 app.get('/', (req, res) => { // yang menerima callback req dan res
   // get akan mengirim response ke browser yang nge request  
-  res.send('Hello semuanya Selalu dihati!')
+  
+  // memanggil database dari tabel
+  db.query("SELECT * FROM mahasiswa", (error, result) => {
+    if (error) {
+        // Mengembalikan response jika terjadi error
+        response(500, null, "Error retrieving data", res);
+    } else {
+        // Mengembalikan response sukses dengan data dari mahasiswa
+        response(200, result, "get all data from mahasiswa", res);
+    }
+  })
+})
+
+// memanggil nim dari database pada route (http://localhost:3000/find?nim=203040020)
+app.get('/find', (req, res) => {
+    console.log('find nim: ', req.query.nim)
+    
+    const sql = `SELECT nama_lengkap FROM mahasiswa WHERE nim = ${req.query.nim}`
+    // masukan ke database
+    db.query(sql, (error, result) => {
+        response(200, result, "find mahasiswa name", res)
+    })
 })
 
 app.post('/login', (req, res) => {
